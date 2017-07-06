5906f50b0a569dc7dd843abe2ed2831089425440
Carlos Nunez <dev@carlosnunez.me>
2017-07-06 01:47:06 -0500

    [main.tf] cnunez | fix logic

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "ucp_manager_b" {
     "aws_security_group.ucp_manager",
     "aws_security_group.ucp_manager_lb"
   ]
-  count = "${var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
+  count = "${var.number_of_aws_availability_zones_to_use > 1 ? 1 : 0}"
   ami = "${data.aws_ami.coreos.id}"
   availability_zone = "${format("%sb", var.aws_region)}" 
   instance_type = "${var.aws_ec2_instance_size}"
