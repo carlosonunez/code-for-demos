a7a258b6bdf73bcf50c52d3f3f8004311ff41cb3
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 22:11:45 -0500

    [modules/aws-route53-hosted_zone/main.tf] cnunez | Inital commit for the route53_hosted_zone module.

---
+++ docker-ddc-terraform/modules/aws-route53-hosted_zone/main.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_zone" "hosted_zone" {
+  name = "${join(".", list(var.aws_route53_environment_name,var.aws_route53_zone_name))}"
+  comment = "${var.aws_route53_zone_comment}"
+  tags = {
+    Name = "${var.aws_route53_zone_name}"
+    Environment = "${var.aws_route53_environment_name}"
+  }
+}
