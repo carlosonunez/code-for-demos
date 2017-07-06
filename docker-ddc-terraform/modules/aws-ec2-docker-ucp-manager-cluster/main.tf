5f642f0af1157b2a9f60690df2ff9db7192ed7f2
Carlos Nunez <dev@carlosnunez.me>
2017-07-06 00:47:50 -0500

    [main.tf] cnunez | Added sg for UCP lb and UCP managers.

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
@@ -14,6 +14,27 @@ data "aws_ami" "coreos" {
   }
 }
 
-resource "aws_security_group" "ucp_manager_security_group" {
-  name = "ucp_manager_sg"
+resource "aws_security_group" "ucp_manager" {
+  name = "ucp_manager-sg"
+  description = "Security group for UCP managers. Managed by Terraform."
+  vpc_id = "${var.aws_vpc_id}"
+  ingress {
+    self = true
+    from_port = 0
+    to_port = 0
+    protocol = -1
+  }
 }
+
+resource "aws_security_group" "ucp_manager_lb" {
+  depends_on = [ "aws_security_group.ucp_manager" ]
+  name = "ucp-manager-lb-sg"
+  description = "Security group for load balancer in front of UCP managers. Enables external access to UCP. Managed by Terraform."
+  vpc_id = "${var.aws_vpc_id}"
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    security_groups = [ "${aws_security_group.ucp_manager.id}" ]
+}
+
