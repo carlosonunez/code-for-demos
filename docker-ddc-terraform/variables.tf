67755b3ea996ae62ab744a6ff68749ea6f4b9776
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 23:13:44 -0500

    [variables.tf] cnunez | add a variable that accepts a public key

--- docker-ddc-terraform/variables.tf
+++ docker-ddc-terraform/variables.tf
@@ -9,3 +9,7 @@ variable "dns_zone_to_use" {
 variable "environment_name" {
   description = "The name of the environment being deployed."
 }
+
+variable "environment_public_key" {
+  description = "The public key to use for this environment. Ensure that its corresponding private key is stored somewhere where you won't lose it!"
+}
