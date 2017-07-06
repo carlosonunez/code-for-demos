c0b8946850acf457f2cdb31be749afb20ce2af27
Carlos Nunez <dev@carlosnunez.me>
2017-07-06 00:30:59 -0500

    [main.tf] cnunez | vpc now requires a terraform deployer ID and a zone id.

--- docker-ddc-terraform/main.tf
+++ docker-ddc-terraform/main.tf
@@ -1,9 +1,7 @@
-module "vpc" {
-  source = "./modules/aws-vpc"
-  aws_vpc_cidr_block = "${var.aws_vpc_cidr_block}"
-  aws_vpc_dns_domain = "${var.dns_zone_to_use}"
+module "ec2_key" {
+  source = "./modules/aws-ec2-key"
   aws_environment_name = "${var.environment_name}"
-  terraform_deployer_ip = "${var.terraform_deployer_ip}"
+  aws_ec2_public_key = "${var.environment_rsa_public_key}"
 }
 
 module "dns_zone" {
@@ -12,13 +10,22 @@ module "dns_zone" {
   aws_environment_name = "${var.environment_name}"
 }
 
-module "ec2_key" {
-  source = "./modules/aws-ec2-key"
+module "vpc" {
+  depends_on = [ "dns_zone" ]
+  source = "./modules/aws-vpc"
+  aws_vpc_cidr_block = "${var.aws_vpc_cidr_block}"
+  aws_vpc_dns_domain = "${var.dns_zone_to_use}"
   aws_environment_name = "${var.environment_name}"
-  aws_ec2_public_key = "${var.environment_rsa_public_key}"
+  terraform_deployer_ip = "${var.terraform_deployer_ip}"
+  aws_route53_zone_id = "${module.dns_zone.id}"
 }
 
 module "ucp_manager-cluster" {
+  depends_on = [
+    "dns_zone",
+    "vpc",
+    "ec2_key"
+  ]
   source = "./modules/aws-ec2-docker-ucp-manager-cluster"
   aws_inbound_ssh_security_group_id = "${module.vpc.ssh_sg_id}"
   aws_vpc_id = "${module.vpc.id}"
