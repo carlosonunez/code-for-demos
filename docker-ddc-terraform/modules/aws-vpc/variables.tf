91aeff1c184a0067d9f7e8817947a8327b6ce100
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 21:48:39 -0500

    [modules/aws-vpc/variables.tf] cnunez | Vars for the AWS-VPC module.

---
+++ docker-ddc-terraform/modules/aws-vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_vpc_cidr_block" {
+  description = "The CIDR block to use for your VPC."
+}
+
+variable "aws_vpc_subdomain" {
+  description = "Subdomain to use for the VPC name. This usually corresponds to the name of your environment."
+}
