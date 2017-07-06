8a18ee090548a462d3386642ca1f94aaa6730938
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 21:58:20 -0500

    [variables.tf] cnunez | variables for our deployment.

---
+++ docker-ddc-terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_vpc_cidr_block" {
+  description = "The VPC CIDR block to use for this environment."
+}
+
+variable "dns_zone_to_use" {
+  description = "The public DNS zone into which deployed hosts will reside."
+  default = "carlosnunez.me"
+}
+
+variable "environment_name" {
+  description = "The name of the environment being deployed."
+  default = "sandbox"
+}
