a2cefb571cc51022c541f2798a898d9fffe2a756
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 22:47:46 -0500

    [main.tf] cnunez | I meant 'dns_domain'

--- docker-ddc-terraform/main.tf
+++ docker-ddc-terraform/main.tf
@@ -1,6 +1,11 @@
 module "vpc" {
   source = "./modules/aws-vpc"
   aws_vpc_cidr_block = "${var.aws_vpc_cidr_block}"
-  aws_vpc_domain = "${var.dns_zone_to_use}"
+  aws_vpc_dns_domain = "${var.dns_zone_to_use}"
   aws_vpc_environment_name = "${var.environment_name}"
+}
+
+module "dns_zone" {
+  source = "./modules/aws-route53-hosted_zone"
+  zone_name = "${var.dns_zone_to_use}"
 }
