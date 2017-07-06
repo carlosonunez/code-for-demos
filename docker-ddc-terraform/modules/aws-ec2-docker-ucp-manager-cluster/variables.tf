1525134f4f4b62b3a5ea5a22115e2f51beb948fe
Carlos Nunez <dev@carlosnunez.me>
2017-07-06 01:14:17 -0500

    [variables.tf] cnunez | fix wrong var name

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/variables.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/variables.tf
@@ -10,7 +10,7 @@ variable "aws_route53_zone_id" {
   description = "The Route 53 zone from which records for this cluster will be hosted."
 }
 
-variable "number_of_availability_zones_to_use" {
+variable "number_of_aws_availability_zones_to_use" {
   description = "The number of AZs to use. The AZs selected for each UCP manager will be in ascending order based on the count provided, e.g. '1' = 'region-a', '2' = ['region-a','region-b'], etc. As such, at least one manager will always be deployed onto AZ 'a'."
 }
 
