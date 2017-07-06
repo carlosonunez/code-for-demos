e4b2afbb661ce9d5e26e757f47f3336b9d21d856
Carlos Nunez <dev@carlosnunez.me>
2017-07-05 23:17:42 -0500

    [modules/aws-ec2-key/main.tf] cnunez | initial commit of aws-ec2-key module.

---
+++ docker-ddc-terraform/modules/aws-ec2-key/main.tf
@@ -0,0 +1,8 @@
+resource "aws_key_pair" "ec2_key" {
+  key_name = "${var.aws_environment_name}"
+  public_key = "${var.aws_ec2_public_key}"
+}
+
+output "ec2_key" {
+  key_name = "${aws_key_pair.ec2_key.key_name}"
+}
