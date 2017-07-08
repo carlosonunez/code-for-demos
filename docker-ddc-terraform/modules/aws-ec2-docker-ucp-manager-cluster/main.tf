dcb8a45e5da0c80054937044970a2d00ee68011e
Carlos Nunez <dev@carlosnunez.me>
2017-07-08 08:54:58 -0500

    [modules/aws-ec2-docker-ucp-manager-cluster/main.tf] cnunez | add a few dependencies.

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
@@ -121,6 +121,9 @@ into the elb resource.
 So we hack around it by creating three load balancer resources and setting a condition
 on which one to use depending on the number_of_azs_to_use parameter. */
 resource "aws_elb" "ucp_manager_elb_single_az" {
+  depends_on = [
+    "aws_instance.ucp_manager_a"
+  ]
   count = "${var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
   name = "ucp-manager-lb"
   availability_zones = ["${format("%sa", var.aws_region)}"]
@@ -148,6 +151,10 @@ resource "aws_elb" "ucp_manager_elb_single_az" {
 }
 
 resource "aws_elb" "ucp_manager_elb_dual_az" {
+  depends_on = [
+    "aws_instance.ucp_manager_a",
+    "aws_instance.ucp_manager_b"
+  ]
   count = "${var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
   availability_zones = ["${format("%sa", var.aws_region)}", 
                         "${format("%sb", var.aws_region)}"]
@@ -175,6 +182,11 @@ resource "aws_elb" "ucp_manager_elb_dual_az" {
 }
 
 resource "aws_elb" "ucp_manager_elb_tri_az" {
+  depends_on = [
+    "aws_instance.ucp_manager_a",
+    "aws_instance.ucp_manager_b",
+    "aws_instance.ucp_manager_c"
+  ]
   count = "${var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
   availability_zones = ["${format("%sa", var.aws_region)}",
                         "${format("%sb", var.aws_region)}",
