23f54c1996ae3105d0c2f55a36eed0b732742958
Carlos Nunez <dev@carlosnunez.me>
2017-07-08 09:19:51 -0500

    [modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf] cnunez | syntax error

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf
@@ -1,11 +1,11 @@
 output "ucp_manager_elb_id" {
   value = "${compact(list(aws_elb.ucp_manager_elb_single_az.id,
                   aws_elb.ucp_manager_elb_dual_az.id,
-                  aws_elb.ucp_manager_elb_tri_az.id)})"
+                  aws_elb.ucp_manager_elb_tri_az.id))}"
 }
 
 output "ucp_manager_elb_dns_name" {
   value = "${compact(list(aws_elb.ucp_manager_elb_single_az.dns_name,
                   aws_elb.ucp_manager_elb_dual_az.dns_name,
-                  aws_elb.ucp_manager_elb_tri_az.dns_name)})"
+                  aws_elb.ucp_manager_elb_tri_az.dns_name))}"
 }
