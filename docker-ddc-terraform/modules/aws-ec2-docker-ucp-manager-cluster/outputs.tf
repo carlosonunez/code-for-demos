e44d4a7f0ba00c6985a3e7cc3e7fcd51d5c90bd8
Carlos Nunez <dev@carlosnunez.me>
2017-07-08 08:30:41 -0500

    [modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf] cnunez | Outputs for this module.

---
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf
@@ -0,0 +1,26 @@
+output "ucp_manager_lb_id" {
+  count = "${var.var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_single_az.id}"
+}
+output "ucp_manager_lb_dns_name" {
+  count = "${var.var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_single_az.dns_name}"
+}
+output "ucp_manager_lb_id" {
+  count = "${var.var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_dual_az.id}"
+}
+
+output "ucp_manager_lb_dns_name" {
+  count = "${var.var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_dual_az.dns_name}"
+}
+
+output "ucp_manager_lb_id" {
+  count = "${var.var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_tri_az.id}"
+}
+output "ucp_manager_lb_dns_name" {
+  count = "${var.var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
+  value = "${aws_elb.ucp_manager_elb_tri_az.dns_name}"
+}
