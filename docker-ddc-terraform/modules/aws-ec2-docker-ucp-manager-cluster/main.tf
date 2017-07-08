dcc2674530daf714935d61d090eea6713b6f532b
Carlos Nunez <dev@carlosnunez.me>
2017-07-08 07:35:24 -0500

    [/home/cnunez/src/code-for-demos/docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf] cnunez | fix syntax within interpolation.

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/main.tf
@@ -122,18 +122,18 @@ on which one to use depending on the number_of_azs_to_use parameter. */
 resource "aws_elb" "ucp_manager_elb_single_az" {
   count = "${var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
   name = "ucp-manager-lb"
-  availability_zones = ["format("%sa", var.aws_region)"]
+  availability_zones = ["${format("%sa", var.aws_region)}"]
 }
 
 resource "aws_elb" "ucp_manager_elb_dual_az" {
   count = "${var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
-  availability_zones = ["format("%sa", var.aws_region)", 
-                        "format("%sb", var.aws_region)"]
+  availability_zones = ["${format("%sa", var.aws_region)}", 
+                        "${format("%sb", var.aws_region)}"]
 }
 
 resource "aws_elb" "ucp_manager_elb_tri_az" {
   count = "${var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
-  availability_zones = ["format("%sa", var.aws_region)",
-                        "format("%sb", var.aws_region)",
-                        "format("%sc", var.aws_region)"]
+  availability_zones = ["${format("%sa", var.aws_region)}",
+                        "${format("%sb", var.aws_region)}",
+                        "${format("%sc", var.aws_region)}"]
 }
