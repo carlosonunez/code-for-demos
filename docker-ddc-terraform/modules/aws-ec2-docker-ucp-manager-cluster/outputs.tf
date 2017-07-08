dcc6065038cda9edbf20a70d4a44f808107f24aa
Carlos Nunez <dev@carlosnunez.me>
2017-07-08 08:34:32 -0500

    [modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf] cnunez | I have to enumerate all aws_elb resources and select the first one because we don't know ahead of time which kind of elb we'll receive.

--- docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf
+++ docker-ddc-terraform/modules/aws-ec2-docker-ucp-manager-cluster/outputs.tf
@@ -1,26 +1,7 @@
-output "ucp_manager_lb_id" {
-  count = "${var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_single_az.id}"
-}
-output "ucp_manager_lb_dns_name" {
-  count = "${var.number_of_aws_availability_zones_to_use <= 1 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_single_az.dns_name}"
-}
-output "ucp_manager_lb_id" {
-  count = "${var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_dual_az.id}"
+output "ucp_manager_elb_id" {
+  value = "${element(aws_elb.*.id, 1)}"
 }
 
-output "ucp_manager_lb_dns_name" {
-  count = "${var.number_of_aws_availability_zones_to_use == 2 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_dual_az.dns_name}"
-}
-
-output "ucp_manager_lb_id" {
-  count = "${var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_tri_az.id}"
-}
-output "ucp_manager_lb_dns_name" {
-  count = "${var.number_of_aws_availability_zones_to_use == 3 ? 1 : 0}"
-  value = "${aws_elb.ucp_manager_elb_tri_az.dns_name}"
+output "ucp_manager_elb_dns_name" {
+  value = "${element(aws_elb.*.dns_name, 1)}"
 }
