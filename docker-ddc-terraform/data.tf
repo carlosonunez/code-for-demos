ac907d23da0fd67479d27efed64106870aa70662
Carlos Nunez <carlos.a.nunez@adp.com>
2017-07-15 15:18:48 -0500

    cnunez | escape the slash maybe?

--- docker-ddc-terraform/data.tf
+++ docker-ddc-terraform/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "this_environment" {
   backend = "s3"
   config {
     bucket = "${var.aws_s3_infrastructure_bucket}"
-    key = "${join("/",list("tfstate",var.environment_name))}"
+    key = "${join("//",list("tfstate",var.environment_name))}"
     region = "${var.aws_region}"
   }
 }
