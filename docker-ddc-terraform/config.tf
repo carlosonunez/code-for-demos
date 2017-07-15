0b37767b44f946f013b99f6d77442e2afdeb012e
Carlos Nunez <carlos.a.nunez@adp.com>
2017-07-15 15:11:14 -0500

    [config.tf] cnunez |  Configure Terraform to use S3 as its remote state.

---
+++ docker-ddc-terraform/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "${var.aws_s3_infrastructure_bucket}"
+    key = "${join('/',list("tfstate",var.environment_name)}"
+    region = "${var.aws_region}"
+  }
+}
