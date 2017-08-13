dd8ada5e3b2ed2858bec75a8604edfec9238e170
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-13 13:23:40 -0500

    [metadata.rb] cnunez |  Make the separation between base and CentOS more clear.

---
+++ deploying-nginx-with-packer-terraform-consul/packer/cookbooks/base/metadata.rb
@@ -0,0 +1,7 @@
+name 'base'
+maintainer 'Carlos Nunez'
+maintainer_email 'carlos.nunez@contino.io'
+description 'Cookbook for provisioning operating systems.'
+version '0.0.1'
+
+recipe 'base::centos'
