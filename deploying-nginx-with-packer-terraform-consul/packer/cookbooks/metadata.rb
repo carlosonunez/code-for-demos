720915177a25aad9c30d80b4abcd7ac84f847c6e
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-13 13:24:35 -0500

    cnunez | moved metadata.

--- deploying-nginx-with-packer-terraform-consul/packer/cookbooks/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'nginx_centos-7'
-maintainer 'Carlos Nunez'
-maintainer_email 'carlos.nunez@contino.io'
-description 'Provisions a CentOS host and installs NGINX.'
-version '0.0.1'
-
-recipe 'base'
-recipe 'nginx'
