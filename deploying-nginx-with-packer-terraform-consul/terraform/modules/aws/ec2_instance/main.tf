8010adad6194af2ceaf80a1e18c23ce5cd311c54
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:12:10 +0000

    [modules/aws/ec2_instance/main.tf] ubuntu |  create our horribly insecure web server security group.

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
@@ -1,3 +1,21 @@
+resource "aws_security_group" "web_servers" {
+  name = "${format("web_server-%s-sg",var.aws_environment)}"
+  description = "Security group description for web servers. Completely open for our demo. DON'T DO THIS FOR REAL ENVIRONMENTS."
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+	}
+}
+
 resource "aws_instance" "instance" {
   ami = "${var.ami_id}"
   instance_type = "${var.instance_type}"
@@ -8,4 +26,5 @@ resource "aws_instance" "instance" {
   root_block_device {
     volume_size = "${var.disk_size}"
   }
+  vpc_security_group_ids = [ "${aws_security_group.web_servers.id}" ]
 }
