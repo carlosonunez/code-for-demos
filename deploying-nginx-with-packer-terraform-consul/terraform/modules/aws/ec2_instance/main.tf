9d4f764a4ddae312e75ed5ca916bf3edcc626e04
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:15:06 +0000

    [modules/aws/ec2_instance/main.tf] ubuntu |  oh, this was there twice.

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
@@ -19,14 +19,11 @@ resource "aws_security_group" "web_servers" {
 resource "aws_instance" "instance" {
   ami = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  vpc_security_group_ids = "${var.security_groups}"
+  vpc_security_group_ids = ["${aws_security_group.web_servers.id}"]
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
   subnet_id = "${var.subnet_id}"
   root_block_device {
     volume_size = "${var.disk_size}"
   }
-  vpc_security_group_ids = [
-    "${aws_security_group.web_servers.id}"
-  ]
 }
