737fe444249ac41bc134631425c99265f9140b03
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:16:43 +0000

    [modules/aws/ec2_instance/outputs.tf] ubuntu |  wrong syntax.

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/outputs.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/outputs.tf
@@ -1,5 +1,11 @@
 output "instance_id" {
   value = "${aws_instance.instance.id}"
-  availability_zone = "${aws_instance.instance.availability_zone}"
-  public_dns = "${aws_instance.instance.public_dns}"
+}
+
+output "availability_zone" {
+  value = "${aws_instance.instance.availability_zone}"
+}
+
+output "public_dns_address" {
+  value = "${aws_instance.instance.public_dns}"
 }
