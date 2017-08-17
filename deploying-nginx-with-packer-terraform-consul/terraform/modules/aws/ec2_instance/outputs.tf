fe3edef6854facc69a58789cb4865001cd18e321
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 03:56:45 +0000

    [outputs.tf] ubuntu |  Create some variables for our new aws_instance module.

---
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/outputs.tf
@@ -0,0 +1,34 @@
+variable "ami_id" {
+  description = "The AMI to provision this instance with."
+}
+
+variable "instance_type" {
+  description = "The instance type to apply onto this EC2 instance."
+}
+
+variable "key_name" {
+  description = "The EC2 public key to use for logging into this instance."
+}
+
+variable "security_groups" {
+  description = "A list of security groups to assign this instance to."
+}
+
+variable "vpc_id" {
+  description = "The VPC to provision this instance into."
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet to provision this instance into."
+}
+
+variable "aws_environment" {
+  description = "The environment into which this instance will be categorized."
+}
+
+variable "disk_size" {
+  description = "The size of the disk to give this instance."
+  default = 8
+}
+
+
