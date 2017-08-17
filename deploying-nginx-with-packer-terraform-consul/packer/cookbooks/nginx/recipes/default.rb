1345e62e254424c0662fe1ec0236201b119bf656
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 02:28:43 +0000

    [cookbooks/nginx/recipes/default.rb] ubuntu |  Wrong directory nam.e

--- deploying-nginx-with-packer-terraform-consul/packer/cookbooks/nginx/recipes/default.rb
+++ deploying-nginx-with-packer-terraform-consul/packer/cookbooks/nginx/recipes/default.rb
@@ -6,7 +6,11 @@ service 'nginx' do
   action [:enable, :start]
 end
 
-cookbook_file "/usr/share/nginx/www/index.html" do
+directory '/usr/share/nginx/html' do
+  action :create
+end
+
+cookbook_file "/usr/share/nginx/html/index.html" do
   source 'index.html'
   mode '0644'
 end
