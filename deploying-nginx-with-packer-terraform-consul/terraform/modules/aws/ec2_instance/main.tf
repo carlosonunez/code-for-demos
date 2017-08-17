8e2d1852085a53b219cde0e0b113727886d8f058
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 05:07:16 +0000

    [modules/aws/ec2_instance/main.tf] ubuntu |  forgot to enable public IP

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
@@ -46,6 +46,7 @@ resource "aws_instance" "instance" {
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
   subnet_id              = "${var.subnet_id}"
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size = "${var.disk_size}"
