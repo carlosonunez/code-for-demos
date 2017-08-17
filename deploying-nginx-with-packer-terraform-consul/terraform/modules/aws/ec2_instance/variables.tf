bc87b844085da7b3bf2575e4b8eb98441d826207
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:06:50 +0000

    cnunez | name swap

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/variables.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/variables.tf
@@ -1,5 +1,30 @@
-output "instance_id" {
-  value = "${aws_instance.instance.id}"
-  availability_zone = "${aws_instance.instance.availability_zone}"
-  public_dns = "${aws_instance.instance.public_dns}"
+variable "ami_id" {
+  description = "The AMI to provision this instance with."
 }
+
+variable "instance_type" {
+  description = "The instance type to apply onto this EC2 instance."
+}
+
+variable "key_name" {
+  description = "The EC2 public key to use for logging into this instance."
+}
+
+variable "vpc_id" {
+  description = "The VPC to provision this instance into."
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet to provision this instance into."
+}
+
+variable "aws_environment" {
+  description = "The environment into which this instance will be categorized."
+}
+
+variable "disk_size" {
+  description = "The size of the disk to give this instance."
+  default = 8
+}
+
+
