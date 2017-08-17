02c3b689133acd6075223607d1b8704e64db128f
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:22:51 +0000

    [modules/aws/ec2_instance/main.tf] ubuntu |  wire up to main

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/main.tf
@@ -18,6 +18,7 @@ resource "aws_security_group" "web_servers" {
 }
 
 resource "aws_instance" "instance" {
+  count                  = "${var.count}"
   ami                    = "${var.ami_id}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${aws_security_group.web_servers.id}"]
