a49b6570f79f8186ae5c1d2a64520168616d9ed7
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-17 04:05:53 +0000

    [main.tf] ubuntu |  complete our web server module.

--- deploying-nginx-with-packer-terraform-consul/terraform/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/main.tf
@@ -13,3 +13,23 @@ module "management-subnet" {
   enable_internet_access  = "true"
   aws_internet_gateway_id = "${module.vpc.internet_gateway_id}"
 }
+
+module "public_instances" {
+  source                  = "./modules/aws/subnet"
+  subnet_name             = "public_instances"
+  vpc_id                  = "${module.vpc.vpc_id}"
+  cidr_block              = "${var.aws_public_instances_subnet_cidr_block}"
+  aws_environment         = "${var.environment_name}"
+  enable_internet_access  = "true"
+  aws_internet_gateway_id = "${module.vpc.internet_gateway_id}"
+}
+
+module "web_server" {
+  source = "./modules/aws/ec2_instance"
+  ami_id = "${var.web_server_ami_id}"
+  instance_type = "${var.web_server_instance_type}"
+  key_name = "${var.environment_name}"
+  vpc_id = "${module.vpc.vpc_id}"
+  subnet_id = "${module.public_instances.subnet_id}"
+  aws_environment = "${var.environment_name}"
+}
