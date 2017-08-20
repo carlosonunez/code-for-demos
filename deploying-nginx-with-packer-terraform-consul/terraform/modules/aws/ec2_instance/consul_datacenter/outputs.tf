a277ebc18870ff9b09d84c74289d76513b95977b
Carlos Nunez <dev@carlosnunez.me>
2017-08-20 20:07:49 +0000

    [modules/aws/ec2_instance/web/outputs.tf] dev@carlosnunez.me  |  add public IP address as an output.

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/consul_datacenter/outputs.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/consul_datacenter/outputs.tf
@@ -9,3 +9,7 @@ output "availability_zone" {
 output "public_dns_address" {
   value = "${aws_instance.instance.*.public_dns}"
 }
+
+output "public_ip_address" {
+  value = "${aws_instance.instance.*.public_ip}"
+}
