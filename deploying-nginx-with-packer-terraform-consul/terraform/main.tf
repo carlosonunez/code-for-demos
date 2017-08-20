b4e349f8d69ec465b6cc59f6499086ee9c4f37ac
Carlos Nunez <carlos.a.nunez@adp.com>
2017-08-20 05:22:55 +0000

    [main.tf] ubuntu |  Add our Consul datacenter configuration into our main.tf

--- deploying-nginx-with-packer-terraform-consul/terraform/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/main.tf
@@ -29,8 +29,19 @@ module "public_instances_subnet" {
   aws_internet_gateway_id = "${module.vpc.internet_gateway_id}"
 }
 
+module "consul_datacenter" {
+  source          = "./modules/aws/ec2_instance/consul_server"
+  count           = "${var.number_of_consul_servers}"
+  ami_id          = "${var.consul_server_ami_id}"
+  instance_type   = "${var.consul_server_instance_type}"
+  key_name        = "${var.environment_name}"
+  vpc_id          = "${module.vpc.vpc_id}"
+  subnet_id       = "${module.public_instances_subnet.subnet_id}"
+  aws_environment = "${var.environment_name}"
+}
+
 module "web_server" {
-  source          = "./modules/aws/ec2_instance"
+  source          = "./modules/aws/ec2_instance/web"
   count           = "${var.number_of_web_servers}"
   ami_id          = "${var.web_server_ami_id}"
   instance_type   = "${var.web_server_instance_type}"
