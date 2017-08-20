45ef68b48d38546c34e163a08d5d3a176e3816a7
Carlos Nunez <dev@carlosnunez.me>
2017-08-20 20:08:18 +0000

    [outputs.tf] dev@carlosnunez.me  |  get public IPs instead.

--- deploying-nginx-with-packer-terraform-consul/terraform/outputs.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/outputs.tf
@@ -6,10 +6,10 @@ output "ec2_key" {
   value = "${aws_key_pair.ec2_key_for_environment.key_name}"
 }
 
-output "consul_datacenter_addresses" {
-  value = "${module.consul_datacenter.public_dns_address}"
+output "consul_datacenter_ip" {
+  value = "${module.consul_datacenter.public_ip}"
 }
 
-output "web_server_addresses" {
-  value = "${module.web_server.public_dns_address}"
+output "web_server_ip" {
+  value = "${module.web_server.public_ip}"
 }
