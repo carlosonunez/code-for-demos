3ae086e4f9cebc91bb4ca057981864d9ff2ea950
Carlos Nunez <dev@carlosnunez.me>
2017-08-20 20:14:36 +0000

    [outputs.tf] dev@carlosnunez.me  |  update name to be more clear about there being multiple

--- deploying-nginx-with-packer-terraform-consul/terraform/outputs.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/outputs.tf
@@ -6,10 +6,10 @@ output "ec2_key" {
   value = "${aws_key_pair.ec2_key_for_environment.key_name}"
 }
 
-output "consul_datacenter_ip_address" {
+output "consul_datacenter_ip_addresses" {
   value = "${module.consul_datacenter.public_ip_address}"
 }
 
-output "web_server_ip_address" {
+output "web_server_ip_addresses" {
   value = "${module.web_server.public_ip_address}"
 }
