b2f88c35de9b774da06036d40a42a9ba85e05840
Carlos Nunez <dev@carlosnunez.me>
2017-08-21 01:23:49 +0000

    [modules/aws/ec2_instance/consul_datacenter/variables.tf] dev@carlosnunez.me  |  We need to pass these in so that we can populate our credentials file.

--- deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/consul_datacenter/variables.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/modules/aws/ec2_instance/consul_datacenter/variables.tf
@@ -2,6 +2,14 @@ variable "ami_id" {
   description = "The AMI to provision this instance with."
 }
 
+variable "aws_access_key" {
+  description = "AWS access key. Used to populate Consul systemd service."
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key. Used to populate Consul systemd service."
+}
+
 variable "instance_type" {
   description = "The instance type to apply onto this EC2 instance."
 }
