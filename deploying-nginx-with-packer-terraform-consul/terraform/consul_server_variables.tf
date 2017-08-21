56ac2e1ad8527bf6f51ba5710da7d8bd57fbf510
Carlos Nunez <dev@carlosnunez.me>
2017-08-21 01:19:10 +0000

    cnunez | lint it up.

--- deploying-nginx-with-packer-terraform-consul/terraform/consul_server_variables.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/consul_server_variables.tf
@@ -1,6 +1,6 @@
 variable "number_of_consul_servers" {
   description = "The number of Consul servers to deploy into your datacenter."
-  default = 5
+  default     = 5
 }
 
 variable "consul_server_ami_id" {
@@ -9,5 +9,5 @@ variable "consul_server_ami_id" {
 
 variable "consul_server_instance_type" {
   description = "The instance type to use for your Consul servers."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
--- deploying-nginx-with-packer-terraform-consul/terraform/main.tf
+++ deploying-nginx-with-packer-terraform-consul/terraform/main.tf
@@ -2,7 +2,8 @@ module "vpc" {
   // Ideally, modules would be in their own separate repositories to avoid needing to
   // copy things locally. However, for this demo, we can get away with
   // using the filesystem.
-  source          = "./modules/aws/vpc"
+  source = "./modules/aws/vpc"
+
   cidr_block      = "${var.aws_vpc_cidr_block}"
   aws_environment = "${var.environment_name}"
 }
@@ -33,14 +34,14 @@ module "public_instances_subnet" {
 }
 
 module "consul_datacenter" {
-  source          = "./modules/aws/ec2_instance/consul_datacenter"
-  count           = "${var.number_of_consul_servers}"
-  ami_id          = "${var.consul_server_ami_id}"
-  instance_type   = "${var.consul_server_instance_type}"
-  key_name        = "${var.environment_name}"
-  vpc_id          = "${module.vpc.vpc_id}"
-  subnet_id       = "${module.public_instances_subnet.subnet_id}"
-  aws_environment = "${var.environment_name}"
+  source               = "./modules/aws/ec2_instance/consul_datacenter"
+  count                = "${var.number_of_consul_servers}"
+  ami_id               = "${var.consul_server_ami_id}"
+  instance_type        = "${var.consul_server_instance_type}"
+  key_name             = "${var.environment_name}"
+  vpc_id               = "${module.vpc.vpc_id}"
+  subnet_id            = "${module.public_instances_subnet.subnet_id}"
+  aws_environment      = "${var.environment_name}"
   private_key_location = "${var.private_key_location}"
 }
 
