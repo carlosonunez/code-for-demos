7085d1c69df0050d0ff54986b4b91e00676493ad
Carlos Nunez <dev@carlosnunez.me>
2017-09-12 03:30:14 +0000

    [backend.tf] dev@carlosnunez.me  |  Move our backend to Atlas to avoid making our S3 world-readable.

--- terraform-enterprise/terraform/backend.tf
+++ terraform-enterprise/terraform/backend.tf
@@ -1,3 +1,13 @@
 terraform {
-  backend "s3" {}
+  // We would usually use something like S3 for remote state. However,
+  // Terraform Enterprise is a fully-managed solution that obviates this.
+  // While Terraform Enterprise can leverage S3-backed remote states,
+  // they would need to be made internet-readable, which is a
+  // large security risk.
+  //
+  // Therefore, we're going to change our remote state backend to Atlas
+  // (Terraform Enterprise).
+  backend "atlas" {
+    name = "carlosonunez/simple_tier_webapp"
+  }
 }
