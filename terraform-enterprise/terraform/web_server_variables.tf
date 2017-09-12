1a5f5d87d96a199e377dc37c423597199fa4c118
Carlos Nunez <dev@carlosnunez.me>
2017-09-12 04:32:35 +0000

    [web_server_variables.tf] dev@carlosnunez.me  |  No longer needed.

--- terraform-enterprise/terraform/web_server_variables.tf
+++ terraform-enterprise/terraform/web_server_variables.tf
@@ -1,7 +1,3 @@
-variable "web_server_ami_id" {
-  description = "The AMI to use for our web servers."
-}
-
 variable "web_server_instance_type" {
   description = "The instance size to use for our web servers."
   default     = "t2.micro"
