9fcb7a0004ca74fb47e536c67fb2a204c37847e0
Carlos Nunez <dev@carlosnunez.me>
2017-09-12 04:32:28 +0000

    [consul_server_variables.tf] dev@carlosnunez.me  |  No longer needed.

--- terraform-enterprise/terraform/consul_server_variables.tf
+++ terraform-enterprise/terraform/consul_server_variables.tf
@@ -3,10 +3,6 @@ variable "number_of_consul_servers" {
   default     = 5
 }
 
-variable "consul_server_ami_id" {
-  description = "The AMI to use for your Consul servers."
-}
-
 variable "consul_server_instance_type" {
   description = "The instance type to use for your Consul servers."
   default     = "t2.micro"
