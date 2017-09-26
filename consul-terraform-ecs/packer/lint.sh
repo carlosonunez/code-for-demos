907e61c7812cf7a362579f519f9989031856bc8c
Carlos Nunez <dev@carlosnunez.me>
2017-09-26 13:52:53 +0000

    [lint.sh]  |  Begin implementing a lint.sh script

---
+++ consul-terraform-ecs/packer/lint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage() {
+  error_message="${@:1}"
+  cat <<EOF
+USAGE: lint.sh [-h|--help] <playbooks-path>
+Lints Ansible playbooks with Docker.
+
+  -h, --help:       Show this usage information.
+  <playbooks-path>: The path to your playbooks (default: ansible-playbooks)
+EOF
+  if [ -z "$error_message" ]
+  then
+    echo "ERROR: $error_message" >&2
+    exit 1
+  fi
+  return 0
+}
+
+show_help="$(echo "$@" | grep -Eq "\-h|\-\-help"; echo $?)"
+if [ "$show_help" -eq 0 ]
+then
+  usage
+  exit 0
+fi
+
+directory_to_lint="$(echo "$@" | sed 's#(-h|--help)##')"
